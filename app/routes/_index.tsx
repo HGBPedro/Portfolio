d210245a6dc4dd0173fd52ea00e739c9c353e53a
Pedro H G Bonel <pedro.bonel@gmail.com>
2023-04-19 01:01:29 -0300

    First page done, second page almost

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,5 +1,7 @@
 import type { V2_MetaFunction } from "@remix-run/react";
 import shared from '../shared.css';
+import Lottie from 'lottie-react'
+import Animation from 'public/animation.json'
 
 export function links() {
   return [{ rel: "stylesheet", href: shared }];
@@ -11,35 +13,40 @@ export const meta: V2_MetaFunction = () => {
 
 export default function Index() {
   return (
-    <div>
-      <div className='little-triangle'/>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
-      <div className='big-triangle'/>
-    </div>
+    <main>
+      <div id='#home' className='home'>
+        <div className='home__little-triangle'/>
+        <aside>
+          <h1>I&#39;m Pedro H. G. Bonel,</h1>
+          <h4>Full-stack developer</h4>
+          <span className='home-text'>Born in 1997,</span><br />
+          <span className='home-text'>tinkering with computers since 6yo</span>
+          <div className='home__link-container'>
+            <a href='#projects' className='home__link'>Projects{'->'}</a>
+            <a href='#about' className='home__link'>More info{'->'}</a>
+          </div>
+        </aside>
+        <Lottie animationData={Animation} loop={true} />
+        <div className='home__big-triangle'/>
+      </div>
+      <div id='#projects' className='projects'>
+        <div className='projects__big-triangle'/>
+        <aside className='projects__grid-container'>
+          <div className='projects__grid'>
+            {Array(20).fill('').map(() => (
+              <div className='projects__card'>
+              </div>
+            ))}
+          </div>
+        </aside>
+        <div>
+          <h1>Projects</h1>
+          <span className='home-text'>
+            Here’s a glimpse of some projects I developed on my own. Some are simple,&nbsp;
+            others have some complexity and all the code can be checked at my <a href='https://github.com/HGBPedro?tab=repositories' target='_blank'>GitHub profile</a>.
+          </span>
+        </div>
+      </div>
+    </main>
   );
 }
